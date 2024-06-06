9503fe654ffcaa7cc33fecd512ebbdc3fd425958
edvunge <costaedy18@gmail.com>
2024-06-06 23:49:16 +0100

    newPackage

---
+++ src/main/java/com/treinaweb/trilhaspring/SpringFramework_Fundamentals/Test.java
@@ -0,0 +1,7 @@
+package com.treinaweb.trilhaspring.SpringFramework_Fundamentals;
+
+public class Test {
+	public static void main(String[] args) {
+        System.out.println("teste heelo");
+    }
+}
---
+++ src/main/java/com/treinaweb/trilhaspring/SpringFramework_TemplatesComThymeleaf/Test.java
@@ -0,0 +1,7 @@
+package com.treinaweb.trilhaspring.SpringFramework_TemplatesComThymeleaf;
+
+public class Test {
+	public static void main(String[] args) {
+        System.out.println("teste helo with new package");
+    }
+}
